4d4c3287c89f97c1afa10b0887e7b4777b01b93b
jeff.park <jeff.park@machbase.com>
2022-07-22 17:00:43 +0900

    ✨ 리스트 두개 왔다갔다 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     Hello!
-    <Drag></Drag>
+    <div class="flex-style">
+      <Drag :pDropzoneId="1" :pList="list1"></Drag>
+      <Drag :pDropzoneId="2" :pList="list2"></Drag>
+    </div>
   </div>
 </template>
 
@@ -12,9 +15,64 @@ export default {
   name: 'App',
   components: {
     Drag
+  },
+  data() {
+    return {
+      list1: [
+        {
+          id: '1',
+          title: 'item A',
+          list: 1,
+        },{
+          id: '2',
+          title: 'item B',
+          list: 1,
+        },{
+          id: '3',
+          title: 'item C',
+          list: 1,
+        },{
+          id: '4',
+          title: 'item D',
+          list: 1,
+        },{
+          id: '5',
+          title: 'item E',
+          list: 1,
+        },
+      ],
+      list2: [
+        {
+          id: '6',
+          title: 'item F',
+          list: 2,
+        },{
+          id: '7',
+          title: 'item G',
+          list: 2,
+        },{
+          id: '8',
+          title: 'item H',
+          list: 2,
+        },{
+          id: '9',
+          title: 'item I',
+          list: 2,
+        },{
+          id: '10',
+          title: 'item J',
+          list: 2,
+        },
+      ]
+    };
   }
 }
 </script>
 
-<style>
+<style scoped>
+.flex-style {
+  display: flex;
+  gap: 20px;
+  padding: 0 20px;
+}
 </style>
--- src/components/dragTest.vue
+++ src/components/dragTest.vue
@@ -1,21 +1,16 @@
 <template>
     <div class="drag-wrapper">
-        <div
-            id="list"
-            class="dropzone"
-            :sortable="{
-                onUpdate: sortableMove
-            }"
-        >
+        <div class="dropzone">
             <div 
-                v-for="(item, index) in tempListOne" 
-                :key="item.id"
+                v-for="(item, index) in tempList" 
+                :key="item.id+'-'+index"
+                :id="item.id"
                 draggable="true"
                 @dragstart="startDrag($event, item, index)"
                 @dragend="dragLeave()"
                 @drop="dragEnd($event, index)"
-                @dragover.prevent="overShow(item.id)"
-                @dragenter.prevent="moveOver(index)"
+                @dragover.prevent
+                @dragenter.prevent
                 class="drag-box"
                 :class="{ 
                     'is-dragging': sIsDragging && (item.id === sDraggingId),
@@ -23,7 +18,7 @@
                     // 'move-action-up': isMoveActionUp(sDraggingId, sOverId) && isOver(item.id),
                 }"
             >
-                <div v-if="sOverShow && (item.id === sOverId) && (item.id !== sDraggingId)" class="move-point" />
+                <!-- <div v-if="sOverShow && (item.id === sOverId) && (item.id !== sDraggingId)" class="move-point" /> -->
                 <span>{{ item.title }}</span>
             </div>
         </div>
@@ -33,95 +28,83 @@
 <script>
 export default {
     name: 'dragTest',
+    props: {
+        pList: [],
+        pDropzoneId: Number,
+    },
     data() {
         return { 
-            tempListOne: [
-                {
-                    id: '1',
-                    title: 'item A',
-                    list: 1,
-                },{
-                    id: '2',
-                    title: 'item B',
-                    list: 1,
-                },{
-                    id: '3',
-                    title: 'item C',
-                    list: 1,
-                },{
-                    id: '4',
-                    title: 'item D',
-                    list: 1,
-                },{
-                    id: '5',
-                    title: 'item E',
-                    list: 1,
-                },
-            ],
-            tempListTwo: [],
+            tempList: [],
             sIsDragging: false,
             sDraggingId: null,
             sOverId: null,
-            sDragzone: 0,
             sFrom: -1,
             sTo: -1,
             sOverShow: false,
             sCustomDiv: null,
+            sListId: null,
         }
     },
     methods: {
-        sortableMove: function (aEvent) {
-            this.tempListOne.splice(aEvent.newIndex, 0, this.tempListOne.splice(aEvent.oldIndex, 1)[0])
-        },
         startDrag(event, aItem, aIdx) {
+            console.log('start drag dropzone', this.pDropzoneId)
             event.dataTransfer.dropEffect = 'move';
             event.dataTransfer.effectAllowed = 'move';
             event.dataTransfer.setData('itemId', aItem.id);
+
             // ghost image control
             let sCustomDiv = event.target.cloneNode(true);
             sCustomDiv.style.opacity = 0.01;
+            sCustomDiv.id = 'customdiv';
+            sCustomDiv.className = "drag-box "+aIdx+" "+aItem.id;
             document.body.appendChild(sCustomDiv);
             event.dataTransfer.setDragImage(sCustomDiv, event.offsetX, event.offsetY);
             this.sCustomDiv = sCustomDiv
 
             this.sDraggingId = aItem.id;
-            this.sDragzone = aItem.list;
             this.sFrom = aIdx;
-            console.log(this.sDraggingId)
             this.toggleDragging('over');
         },
         dragEnd(event, aTo) {
             event.preventDefault();
-            console.log('from to = ', this.sFrom, aTo)
-            this.moveItem(this.sFrom, aTo);
+            console.log('drag end dropzone', this.pDropzoneId)
+
+            const sCustomDiv = document.getElementById('customdiv').cloneNode(true);
+            sCustomDiv.id = sCustomDiv.className.split(' ')[2];
+
+            this.moveItem(sCustomDiv.className.split(' ')[1], aTo, sCustomDiv);
+
             this.sFrom = -1;
             this.sTo = -1;
             this.toggleDragging('end');
             this.endOverShow();
             if(this.sCustomDiv){
-                console.log(this.sCustomDiv);
                 document.body.removeChild(this.sCustomDiv);
                 this.sCustomDiv = null;
             }
         },
         dragLeave() {
+            if(this.sCustomDiv){
+                document.body.removeChild(this.sCustomDiv);
+                //여기서 삭제해버리자~
+                this.tempList.splice(this.sFrom, 1)
+                this.sCustomDiv = null;
+            }
             this.sFrom = -1;
             this.sTo = -1;
             this.toggleDragging('end');
             this.endOverShow();
-            if(this.sCustomDiv){
-                console.log(this.sCustomDiv);
-                document.body.removeChild(this.sCustomDiv);
-                this.sCustomDiv = null;
+        },
+        moveItem(aFrom, aTo, aCustomdiv) {
+            if(this.sIsDragging) {
+                this.tempList.splice(aTo, 0, this.tempList.splice(aFrom, 1)[0]);
+            } else {
+                this.tempList.splice(aTo, 0, {
+                    id: aCustomdiv.id,
+                    title: aCustomdiv.innerText,
+                    list: this.pDropzoneId
+                });
             }
-        },
-        moveItem(aFrom, aTo) {
-            this.tempListOne.splice(aTo, 0, this.tempListOne.splice(aFrom, 1)[0]);
-        },
-        moveOver(aTo) {
-            console.log('over from to !', this.sFrom, aTo)
-            this.tempListOne.splice(aTo, 0, this.tempListOne.splice(this.sFrom, 1)[0]);
-            this.sFrom = aTo;
         },
         toggleDragging(aType){
             if(aType === 'over'){
@@ -138,9 +121,6 @@ export default {
             this.sOverId = null;
             this.sOverShow = false;
         },
-        filterList(aListIndex) {
-            return this.items.filter((aItem) => aItem.list === aListIndex);
-        },
         isMoveActionDwon(aFrom, aTo) {
             if(aFrom > aTo){
                 return true;
@@ -155,8 +135,11 @@ export default {
         },
         isOver(aId) {
             return this.sOverShow && (aId === this.sOverId) && (aId !== this.sDraggingId);
-        }
+        },
     },
+    created() {
+        return this.tempList = this.pList
+    }
 }
 </script>
 
